538c144935882a9f72b87057308d3c96fee0c720
Glitch (glitch-hello-node) <none>
2024-10-13 20:00:55 +0000

    🐦💳 Checkpoint ./server.js:70490188/16

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const fs = require("fs");
 
 //Inicialização do banco de dados SQLite
-const dbFile = "./.data/produtos3.db";
+const dbFile = "./.data/pessoas.db";
 const exists = fs.existsSync(dbFile);
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database(dbFile);
