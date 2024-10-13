bf010ad693221aff5f18e9a6c31c8b32ebd48306
Glitch (glitch-hello-node) <none>
2024-10-13 17:33:14 +0000

    🙎🍶 Checkpoint ./package.json:70490188/695 ./server.js:70490188/10655

--- server.js
+++ server.js
@@ -1,119 +1,233 @@
-/**
- * This is the main Node.js server script for your project
- * Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
- */
+//Criar o app, express...
+const express = require("express");
+const app = express();
+const fs = require("fs");
 
-const path = require("path");
+//Inicialização do banco de dados SQLite
+const dbFile = "./.data/produtos2.db";
+const exists = fs.existsSync(dbFile);
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database(dbFile);
 
-// Require the fastify framework and instantiate it
-const fastify = require("fastify")({
-  // Set this to true for detailed logging:
-  logger: false,
-});
+//0.3.0: Chamando jwt, bcryptjs e body-parser
+const jwt = require("jsonwebtoken");
+const bcryptjs = require("bcryptjs");
+const bodyParser = require("body-parser");
 
-// ADD FAVORITES ARRAY VARIABLE FROM TODO HERE
 
-// Setup our static files
-fastify.register(require("@fastify/static"), {
-  root: path.join(__dirname, "public"),
-  prefix: "/", // optional: default '/'
-});
 
-// Formbody lets us parse incoming forms
-fastify.register(require("@fastify/formbody"));
-
-// View is a templating manager for fastify
-fastify.register(require("@fastify/view"), {
-  engine: {
-    handlebars: require("handlebars"),
-  },
-});
-
-// Load and parse SEO data
-const seo = require("./src/seo.json");
-if (seo.url === "glitch-default") {
-  seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
-}
-
-/**
- * Our home page route
- *
- * Returns src/pages/index.hbs with data built into it
- */
-fastify.get("/", function (request, reply) {
-  // params is an object we'll pass to our handlebars template
-  let params = { seo: seo };
-
-  // If someone clicked the option for a random color it'll be passed in the querystring
-  if (request.query.randomize) {
-    // We need to load our color data file, pick one at random, and add it to the params
-    const colors = require("./src/colors.json");
-    const allColors = Object.keys(colors);
-    let currentColor = allColors[(allColors.length * Math.random()) << 0];
-
-    // Add the color properties to the params object
-    params = {
-      color: colors[currentColor],
-      colorError: null,
-      seo: seo,
-    };
+//Se o banco não existir, crie ele primeiro
+db.serialize(() => {
+  if (!exists) { // (exists == false)
+    db.run("CREATE TABLE produtos (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT NOT NULL, preco REAL NOT NULL, estoque INTEGER NOT NULL DEFAULT 0)")
+    console.log("Tabela PRODUTOS criada!");
   }
-
-  // The Handlebars code will be able to access the parameter values and build them into the page
-  return reply.view("/src/pages/index.hbs", params);
-});
-
-/**
- * Our POST route to handle and react to form submissions
- *
- * Accepts body data indicating the user choice
- */
-fastify.post("/", function (request, reply) {
-  // Build the params object to pass to the template
-  let params = { seo: seo };
-
-  // If the user submitted a color through the form it'll be passed here in the request body
-  let color = request.body.color;
-
-  // If it's not empty, let's try to find the color
-  if (color) {
-    // ADD CODE FROM TODO HERE TO SAVE SUBMITTED FAVORITES
-
-    // Load our color data file
-    const colors = require("./src/colors.json");
-
-    // Take our form submission, remove whitespace, and convert to lowercase
-    color = color.toLowerCase().replace(/\s/g, "");
-
-    // Now we see if that color is a key in our colors object
-    if (colors[color]) {
-      // Found one!
-      params = {
-        color: colors[color],
-        colorError: null,
-        seo: seo,
-      };
-    } else {
-      // No luck! Return the user value as the error property
-      params = {
-        colorError: request.body.color,
-        seo: seo,
-      };
-    }
+  else
+  {
+    console.log("Tabela PRODUTOS já existe e funciona bem!");
   }
-
-  // The Handlebars template will use the parameter values to update the page with the chosen color
-  return reply.view("/src/pages/index.hbs", params);
 });
 
-// Run the server and report out to the logs
-fastify.listen(
-  { port: process.env.PORT, host: "0.0.0.0" },
-  function (err, address) {
+//Vamos tratar quando o visitante acessar o "/" (página principal)
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/index.html");
+});
+//se nao colocar isso aqui o post não funciona
+app.use(express.json());
+
+//Serviço para criar tabela de usuários e 2 usuários
+app.get("/criarUsuarios", function(request, response) {
+  db.run("CREATE TABLE IF NOT EXISTS usuarios (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, password TEXT NOT NULL, tipo TEXT NOT NULL, UNIQUE(username))");
+  db.run("INSERT INTO usuarios (username, password, tipo) VALUES ('fulano', '12345', 'user')");
+  db.run("INSERT INTO usuarios (username, password, tipo) VALUES ('admin', 'admin123', 'admin')");
+  return response.status(200).send();
+});
+
+//Função para gerar token JWT
+const generateToken = (user) => {
+  return jwt.sign({id: user.id, username: user.username, tipo:user.tipo}, 'seuSegredoJWT', { expiresIn: '1h'});
+};
+
+//Rota para login de usuário
+app.post('/api/login', (request, response) => {
+  const { username, password } = request.body;
+  
+//Busca o usuário no banco de dados
+db.get('SELECT id, username, password, tipo FROM usuarios WHERE username = ?', [username], (err, user) => {
     if (err) {
-      console.error(err);
-      process.exit(1);
+      return response.status(500).json({ error: 'Erro no banco de dados.' });
     }
-    console.log(`Your app is listening on ${address}`);
+    if (!user) {
+      return response.status(404).json({ error: 'Usuário não encontrado.' });
+    }
+    
+    console.log(password+' '+user.password);
+    //aqui vai entrar criptografia depois..
+    if (password == user.password) {
+      const token = generateToken(user);
+      return response.json({message: "Login bem-sucedido!", token});
+    } else {
+      return response.status(401).json({error: "Senha inválida."});
+    }
+  });
+});
+//Verificar token!!
+const verifyToken = (request, response, next) => {
+  const token = request.headers['x-access-token'];
+  if(!token) { //undefined
+    return response.status(403).json({error: "Nenhum token foi fornecido."});
   }
-);
+  
+  jwt.verify(token, 'seuSegredoJWT', (error, decoded) => {
+    if(error) {
+      return response.status(500).json({error: "Falha ao autenticar o token."});
+    }
+    
+    request.userid = decoded.id;
+    request.usertipo = decoded.tipo;
+    next();  
+  });
+  
+};
+
+// Rota GET para retornar todos os produtos
+app.get("/api/produtos",verifyToken, function(request, response) {
+  //response.json(produtos);
+  db.all("SELECT * FROM produtos", (error, linhas) => {
+    response.setHeader('content-type', 'text/json');
+    return response.send(JSON.stringify(linhas));
+  })
+});
+
+//ROTA GET para retornar um único produto, passando o ID do mesmo na URL
+app.get("/api/produtos/:id",verifyToken , function(request, response) {
+  const produto_id = parseInt(request.params.id)
+  const sql = "SELECT id, nome, preco, estoque FROM produtos WHERE id = ?";
+  db.get(sql, [produto_id], function(error, linha) {
+    if (error) {
+      return response.status(500).send(error);      
+    } else {
+      console.log(linha);
+      if (!linha) {
+        return response.status(404).send("Produto não encontrado"); 
+      } else {
+        response.setHeader('content-type', 'application/json');
+        return response.send(JSON.stringify(linha));
+      }
+    }
+  });
+  /*
+  const produto = produtos.find(i => i.id === produto_id);
+  
+  if (!produto) { // produto == false
+    return response.status(404).send("Produto não encontrado");
+  } else {
+    return response.json(produto);
+  }*/
+});
+  
+//Rota POST para criar um produto...
+app.post("/api/produtos", verifyToken, function(request, response) {
+  if (request.usertipo != 'admin'){
+    return response.status(500).send({error: "Você não tem privilégios para cadastrar produto."});
+  }
+  
+
+  db.run("INSERT INTO produtos (nome, preco, estoque) VALUES (?, ?, ?) ", request.body.nome, request.body.preco, request.body.estoque, function(error){
+  if(error) {
+    return response.status(500).send(error);
+    } else {
+      return response.status(201).json({ id: this.lastID, nome: request.body.nome, preco: request.body.preco, estoque: request.body.estoque});
+    }
+  })
+});
+
+app.patch("/api/produtos", function(request, response) {
+  return response.status(500).send("Erro interno do servidor!");
+});
+
+// ATUALIZAR PRODUTO...
+app.patch("/api/produtos/:id", verifyToken, function(request, response) {
+  if (request.usertipo != 'admin'){
+    return response.status(500).send({error: "Você não tem privilégios para cadastrar produto."});
+  }
+  
+  const produto_id = parseInt(request.params.id);
+  
+  
+  
+  //Passando TUDO, nome, preço, estoque.....
+  let set = "";
+  let valores = [];
+  
+  //Se vai ter nome
+  if(request.body.nome != undefined){
+    set = "nome=?";
+    valores.push(request.body.nome);
+  }
+  
+  //Se vai ter preco
+  if(request.body.preco != undefined){
+    if(set.length > 0) {
+      set += ",";
+      }
+      set += "preco=?";
+      valores.push(request.body.preco);
+    }
+  
+  //Se vai ter estoque
+  if(request.body.estoque != undefined){
+    if(set.length > 0) {
+      set += ",";
+      }
+      set += "estoque=?";
+      valores.push(request.body.estoque);
+    }
+  
+  const sql = "UPDATE produtos SET " +set+ "WHERE id=?";
+  valores.push(produto_id);
+  console.log(sql);
+  
+  db.run(sql, valores, function(error) {
+    if (error) {
+      return response.status(500).send("Erro interno do servidor.");
+    } else {
+      if (this.changes === 0) {
+        return response.status(404).send("Produto não encontrado.");
+      } else {
+        return response.status(200).send();
+      }
+    }
+  });
+});
+
+//APAGAR PRODUTO CORRIGIDO
+
+app.delete("/api/produtos/:id",verifyToken, function(request, response) {
+  if (request.usertipo != 'admin'){
+    return response.status(500).send({error: "Você não tem privilégios para cadastrar produto."});
+  }
+  
+  
+  const produto_id = parseInt(request.params.id);
+  
+  const sql = "DELETE FROM produtos WHERE id=?";
+  db.run(sql, produto_id, function(error) {
+    if(error) {
+      return response.status(500).send("Erro no servidor");
+    } else {
+      if (this.changes === 0) {
+          return response.status(404).send("Produto não encontrado.");
+      } else {
+        return response.status(204).send();
+      }
+    }
+  });
+});
+/*   FIM DO MEU API SERVER      */
+
+
+//"Listener"
+const listener = app.listen(process.env.PORT, function() {
+console.log("Your app is listening on port "+ listener.address().port);
+});
